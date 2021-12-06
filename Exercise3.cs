c8b14359bcfbaa5550e4f9bbb770c580725781cd
Bhgyashree <77121021+BHAGYASHREE-2707@users.noreply.github.com>
2021-12-07 00:23:49 +0530

    Add files via upload

---
+++ Exercise3.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public class Exercise3
+{
+    public static void Main()
+    {
+        System.Console.WriteLine(36 / 6);
+    }
+}
