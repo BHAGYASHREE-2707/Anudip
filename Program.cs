6662f89ad260adff77597793850708a6134cc45b
Bhgyashree <77121021+BHAGYASHREE-2707@users.noreply.github.com>
2021-12-07 00:08:56 +0530

    Delete Program.cs

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-public class Exercise1
-{
-    public static void Main()
-    {
-        System.Console.WriteLine("Hello:Bhagyashree Gedam");
-    }
-}
