bc10eafeb13588972589e9725e3e939888e7082c
Bhgyashree <77121021+BHAGYASHREE-2707@users.noreply.github.com>
2021-12-08 14:03:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+    public class evenoddnumber
+{
+    public static void Main()
+    {
+        int i;
+        Console.WriteLine("Enter the number");
+        i = int.Parse(Console.ReadLine());
+        if(i%2==0)
+        {
+            Console.Write("Entered number is an even Number");
+            Console.Read();
+        }
+        else
+        {
+            Console.Write("Entered Number is an Odd Number");
+            Console.ReadLine();
+        }
+    }
+}
